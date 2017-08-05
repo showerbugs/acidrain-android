7056c16457e0205957539c2f27a61b9c17042b86
daelight <kdaeyoon88@gmail.com>
2017-08-05 20:27:01 +0900

    Link SignUp button

--- app/src/main/java/shwerbugs/com/acidrain/Fragment/SignUpFragment.java
+++ app/src/main/java/shwerbugs/com/acidrain/Fragment/SignUpFragment.java
@@ -3,9 +3,11 @@ package shwerbugs.com.acidrain.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import shwerbugs.com.acidrain.R;
 
@@ -17,6 +19,15 @@ public class SignUpFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.content_signup, container, false);
+        View view = inflater.inflate(R.layout.content_signup, container, false);
+
+        Button signUPBtn = (Button) view.findViewById(R.id.sign_up_confirm);
+        signUPBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        return view;
     }
 }
