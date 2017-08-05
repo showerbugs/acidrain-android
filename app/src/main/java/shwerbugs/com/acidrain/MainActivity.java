3ca2a04222fe95e46a0cd42f5d208c5b07a08e8c
daelight <kdaeyoon88@gmail.com>
2017-08-05 18:10:17 +0900

    Add login layout - 2

--- app/src/main/java/shwerbugs/com/acidrain/MainActivity.java
+++ app/src/main/java/shwerbugs/com/acidrain/MainActivity.java
@@ -1,5 +1,6 @@
 package shwerbugs.com.acidrain;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,6 +13,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -40,6 +42,23 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        Button loginBtn = (Button) findViewById(R.id.login_sign_in);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
+        Button signUPBtn = (Button) findViewById(R.id.login_sign_up);
+        signUPBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 
     @Override
